59eee0d93ad6265f320b6cd7fa904125f80e8681
hadia <hadiamahmood594@gmail.com>
2025-04-25 00:07:18 +0500

    cleaning

--- backend/config/pinataConfig.js
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const PINATA_APIKEY = process.env.PINATA_APIKEY;
-export const PINATA_SECRETKEY = process.env.PINATA_SECRETKEY;
--- backend/middleware/multer.js
+++
@@ -1,5 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage(); // Store file data in memory
-
-export const uploadUserImage = multer({ storage }).single("file");
