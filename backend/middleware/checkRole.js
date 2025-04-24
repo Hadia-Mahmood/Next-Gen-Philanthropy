0a1be8a3af7eb0e7a9eb29fdf542e70a2cd1bfc7
hadia <hadiamahmood594@gmail.com>
2025-04-25 00:04:15 +0500

    frontend branch

--- backend/middleware/checkRole.js
+++
@@ -1,6 +0,0 @@
-export const checkRole = (roles) => async (req, res, next) => {
-    console.log(req.role);
-    !roles.includes(req.role)
-      ? res.status(401).json("Sorry you are not authorized to perform this action")
-      : next();
-  };
